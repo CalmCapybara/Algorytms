b938e378c30774fa29153a1450d14240ce325a8a
Kitavtsev Stanislav <Kitavtsev.s@gmail.com>
2021-09-30 11:36:40 +0300

    StairClass finish. CalculatorClass start. ver.1.2 Finish CalculatorClass

--- src/Dynamic/CalculatorClass.java
+++ src/Dynamic/CalculatorClass.java
@@ -11,35 +11,52 @@ class CalculatorClass {
     void run() {
         Scanner scanner = new Scanner(System.in);
         int i = scanner.nextInt();
-        if (i < 3) {
-            System.out.println("0");
-            System.out.println(i);
+        if (i <= 3) {
+            if (i == 1) {
+                System.out.println("0 \n1");
+                return;
+            }
+            System.out.println("1 \n" + "1 " + i);
+            return;
+
         }
         int[] array = new int[i + 1];
         array[1] = 0;
         array[2] = 1;
         array[3] = 1;
+        int[] prev = new int[i + 1];
+        prev[1] = 0;
+        prev[2] = 1;
+        prev[3] = 1;
+
 
         for (int j = 4; j <= i; j++) {
             int[] counts = new int[3];
             counts[0] = j % 3 == 0 ? array[j / 3] : Integer.MAX_VALUE;
             counts[1] = j % 2 == 0 ? array[j / 2] : Integer.MAX_VALUE;
             counts[2] = array[j - 1];
-            array[j] = Arrays.stream(counts).min().getAsInt() + 1;
+            array[j] = Arrays.stream(counts).min().getAsInt();
+
+            if (array[j] == counts[0]) prev[j] = j / 3;
+            else if (array[j] == counts[1]) prev[j] = j / 2;
+            else prev[j] = j - 1;
+
+            array[j]++;
+
         }
+        int[] answer = new int[array[i] + 1];
         System.out.println(array[i]);
-        int chislo = array[i];
-        int[] result = new int[chislo+1];
-        for (int j = i, i1 = 0; j > 0; i1++) {
-            result[i1] = j;
-            while (array[j] >= chislo) {
-                if (j == 0) break;
-                j--;
-            }
-            chislo--;
+        int j = array[i];
+        answer[j] = i;
+        j--;
+        while (prev[i] != 1) {
+            answer[j] = prev[i];
+            i = prev[i];
+            j--;
         }
-        for (int i1 = result.length - 1; i1 >= 0; i1--) {
-            System.out.print(result[i1] + " ");
+        answer[0] = 1;
+        for (int i1 : answer) {
+            System.out.print(i1 + " ");
         }
 
     }
